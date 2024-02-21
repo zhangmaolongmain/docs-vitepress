b24450fc890938f71dd258be01f38e97aa000670
张茂龙 <zhangmaolong@qinsilk.com>
2024-02-21 20:06:56 +0800

    feature：新建开发环境文档库

--- .vitepress/config.mjs
+++ .vitepress/config.mjs
@@ -9,7 +9,8 @@ export default defineConfig({
     // https://vitepress.dev/reference/default-theme-config
     nav: [
       { text: 'Home', link: '/' },
-      { text: 'Examples', link: '/markdown-examples' }
+      { text: 'Examples', link: '/markdown-examples' },
+      { text: '开发环境', link: '/docs/env/README' }
     ],
 
     sidebar: [
