87e12995b86e1452445fcfd2dc9142c80fea80ae
zhangmaolongmain <zhangmaolongmain@163.com>
2024-02-22 00:50:44 +0800

    style：代码格式化

--- .vitepress/config.mjs
+++ .vitepress/config.mjs
@@ -1,35 +1,35 @@
-import { defineConfig } from 'vitepress'
+import {defineConfig} from 'vitepress'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
-  base: "/docs-vitepress/",
-  title: "zhangmaolongmain",
-  description: "A VitePress Site",
-  themeConfig: {
-    // https://vitepress.dev/reference/default-theme-config
-    logo: "/images/logo.svg",
-    nav: [
-      { text: 'Home', link: '/' },
-      { text: 'Examples', link: '/markdown-examples' },
-      { text: '开发环境', link: '/docs/env/README' }
-    ],
+    base: "/docs-vitepress/",
+    title: "zhangmaolongmain",
+    description: "A VitePress Site",
+    themeConfig: {
+        // https://vitepress.dev/reference/default-theme-config
+        logo: "/images/logo.svg",
+        nav: [
+            {text: 'Home', link: '/'},
+            {text: 'Examples', link: '/markdown-examples'},
+            {text: '开发环境', link: '/docs/env/README'}
+        ],
 
-    sidebar: [
-      {
-        text: 'Examples',
-        items: [
-          { text: 'Markdown Examples', link: '/markdown-examples' },
-          { text: 'Runtime API Examples', link: '/api-examples' }
+        sidebar: [
+            {
+                text: 'Examples',
+                items: [
+                    {text: 'Markdown Examples', link: '/markdown-examples'},
+                    {text: 'Runtime API Examples', link: '/api-examples'}
+                ]
+            }
+        ],
+
+        footer: {
+            copyright: "Copyright@ 2024 zhangmaolong"
+        },
+
+        socialLinks: [
+            {icon: 'github', link: 'https://github.com/vuejs/vitepress'}
         ]
-      }
-    ],
-
-    footer: {
-      copyright: "Copyright@ 2024 zhangmaolong"
-    },
-
-    socialLinks: [
-      { icon: 'github', link: 'https://github.com/vuejs/vitepress' }
-    ]
-  }
+    }
 })
